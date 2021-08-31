4272a99f3042928cce4b879ea02b7bdafcc986ce
nishantgrg21 <nishantgrg21@gmail.com>
2021-08-31 17:12:10 +0530

    Session Storage

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -10,6 +10,26 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    let obj = {fname:"nishant", lname:"garg"}
+
+    localStorage.setItem('Userid', JSON.stringify(obj));       // Set Local Storage Value in JSON format
+    console.log (JSON.parse((localStorage.getItem('Userid') || '{}')));       // Get Local Storage Value in JSON Format
+
+    
+    //localStorage.setItem('UserData', 'Nishant Garg ');   // Set Local Storage Value 
+    localStorage.setItem('EmployeeData', 'Ram Singh  ');    // Set Local Storage Value 
+    sessionStorage.setItem('SessionData', 'SessionStorage');     // Set Session Storage Value
+
+
+    // localStorage.removeItem('LocalData');     // Remove the localStorage
+    // localStorage.clear();        // Remove all the localstorage value 
+
+    //console.log(localStorage.getItem('UserData'));       // Get Local Storage Value
+   console.log(localStorage.getItem('EmployeeData'));       // Get Local Storage Value
+   // console.log(sessionStorage.getItem('SessionData'));   // Get Session Storage Value
+
+    
   }
 
 }
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -10,6 +10,25 @@ export class ServicesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(localStorage.getItem('LocalData')); // Get Local Storage from About us ts file
+    console.log(sessionStorage.getItem('SessionData'));   // Get Session Storage from About us ts file
+    console.log (JSON.parse((localStorage.getItem('Userid') || '{}')));       // Get Local Storage Value in JSON Format
+
+    // Check Session Storage Value is Available and Not Available
+    // if (sessionStorage.length>0) {
+    //   alert("Data Available");
+    // } else {
+    //   alert("Data Not Available");
+    // }
+
+    // Check The Local Storage support on window or not
+    if(window.localStorage){
+      alert("supported");
+    }
+    else{
+      alert("Not supported");
+    }
   }
 
 }
